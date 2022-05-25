7f9d2741325dc1909e78363b16e74cd4e63fab34
cristobalgm <cristobalgonzalezm.97@gmail.com>
2022-05-25 16:43:57 -0500

    Primer commit de los ejercicios de javascript

---
+++ js/main.js
@@ -0,0 +1,112 @@
+// let a = prompt("Inserte numero 1 del 1 al 100");
+// let b = prompt("Inserte numero 2 del 1 al 100");
+// let c = prompt("Inserte numero 3 del 1 al 100");
+
+// if (a>b && a>c){
+//     console.log("El numero mayor de los tres es " + a)
+// } else if
+//     (b>a && b>c)
+//     console.log("El numero mayor de los tres es " +b)
+ 
+//     else{
+//         console.log("El numero mayor de los tres es " +c)
+// }
+    
+
+// let a = prompt("Inserte numero 1 del 1 al 100");
+// // if(a<1 || a>100){
+// //     console.log("Tu primer número no entra en el rango");
+// // }
+// let b = prompt("Inserte numero 2 del 1 al 100");
+// let c = prompt("Inserte numero 3 del 1 al 100");
+
+// if (a<b && a<c){
+//     console.log("El numero menor de los tres es " + a)
+// } else if
+//     (b<a && b<c)
+//     console.log("El numero menor de los tres es " +b)
+ 
+//     else{
+//         console.log("El numero menor de los tres es " +c)
+// }
+    
+// let a = prompt("Inserte un numero del 100 al 200");
+
+// if (a%3==0) {
+//     console.log("El numero si es multiplo de 3");
+// }    else{
+//     console.log("El numero no es multiplo de 3");
+//     }
+    
+// let a =parseFloat(prompt("Inserte numero a")); //parseFloat para convertir la cadena en numerico y poder sumarlos
+// let b = parseFloat(prompt("Inserte numero b"));
+// let c = parseFloat(prompt("Inserte numero c"));
+
+// let d = b+c;
+// let e = a+c;
+// let f = a+b;
+
+// if (a==d) {
+//     console.log("El numero " + a +" es la suma de b y c")
+    
+// } else if
+//     (b==e)
+//     console.log("El numero "+b+" es la suma de a + c")
+
+//     else if
+//     (c==f)
+//     console.log("El numero "+c+" es la suma de a + b")
+
+//     else{
+//         console.log("Ningun numero es resultado de la suma de los otros numeros")
+//     }
+
+// let f=parseFloat(prompt("Dame un número"));
+// let g=parseFloat(prompt("Dame otro número"));
+// let h=parseFloat(prompt("Dame tu numero final"));
+
+// let j = f+g;
+// let k = f+h;
+// let l = g+h;
+// if(f==l){
+//     console.log("Tu número",f,"Es la suma de:",g,"más",h);
+// }else if(g==k){
+//     console.log("Tu número,",g,"es la suma de:",f,"más",h);
+// }else if(h==j){
+//     console.log("Tu número,",h,"es la suma de:",f,"más",g);
+// }else{
+//     console.log("Tus numeros no son suma de los otros");
+// }
+
+// let a=parseFloat(prompt("Inserte un numero"))
+
+// if(a%2==0){
+//     console.log("El numero "+a+" es par")
+// } else{
+//     console.log("El numero "+a+" es impar")
+// }
+
+let a = prompt("Inserte numero a"); 
+let b = prompt("Inserte numero b");
+let c = prompt("Inserte numero c");
+
+console.log("El numero a "+ a +" El numero "+ b +" El numero "+ c );
+
+// let d = b+c;
+// let e = a+c;
+// let f = a+b;
+
+// if (a==d) {
+//     console.log("El numero " + a +" es la suma de b y c")
+    
+// } else if
+//     (b==e)
+//     console.log("El numero "+b+" es la suma de a + c")
+
+//     else if
+//     (c==f)
+//     console.log("El numero "+c+" es la suma de a + b")
+
+//     else{
+//         console.log("Ningun numero es resultado de la suma de los otros numeros")
+//     }
